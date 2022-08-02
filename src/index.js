9abab6b467061b9d098a989c22f5d1b5d393247e
Oleksandr Serebriakov <sealex1977@gmail.com>
2022-08-03 01:26:14 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+// '([{}])'
+// [['(', ')'], ['[', ']'], ['{', '}']]
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 !== 0 ) {
+    return false;
+  }
+
+  const arrBrackets = bracketsConfig.map(index => index.join(''));
+
+  for (let i = 0; i < arrBrackets.length; i++) {
+    if (str.includes(arrBrackets[i])) {
+      str = str.replace(arrBrackets[i], '');
+      i = -1;
+    }
+  };
+  
+  return !str;
 }
